5b23746ea4f0ab647bb5686c73eda60bf3897a55
Przemyslaw <przemyslaw.kmiecik@etendard.pl>
2020-07-05 15:38:48 +0200

    admin view moved to /admin in views folder. AppAbstractController created

---
+++ src/main/java/com/nordgeo/controller/app/dashboard/DashboardControllerApp.java
@@ -0,0 +1,33 @@
+package com.nordgeo.controller.app.dashboard;
+
+
+import com.nordgeo.controller.AppAbstractController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+@RequestMapping(value = "")
+public class DashboardControllerApp extends AppAbstractController {
+
+
+    @ModelAttribute("moduleBaseUrl")
+    public String moduleBaseUrl() {
+        return "/dashboard";
+    }
+
+    @ModelAttribute("title")
+    public String setTitle() {
+        return "user.title";
+    }
+
+    @RequestMapping("/dashboard")
+    public String index( HttpSession session) {
+        session.invalidate();
+
+        return "dashboard.index";
+    }
+}
