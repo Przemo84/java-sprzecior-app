8164408b580a0c1e12c455ddcf12dfc781b934cb
Przemyslaw <przemyslaw.kmiecik@etendard.pl>
2020-07-05 15:27:47 +0200

    commented code removed in PasswordConstraintValidator

--- src/main/java/com/nordgeo/validation/validator/PasswordConstraintValidator.java
+++ src/main/java/com/nordgeo/validation/validator/PasswordConstraintValidator.java
@@ -13,21 +13,15 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
     public boolean isValid(String password, ConstraintValidatorContext context) {
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
-                new DigitCharacterRule(1)
-//                      new UppercaseCharacterRule(1),
-//                      new NumericalSequenceRule(3,false),
-//                      new AlphabeticalSequenceRule(3,false),
-//                      new QwertySequenceRule(3,false),
-//                      new WhitespaceRule()
-        )
+                new DigitCharacterRule(1))
         );
 
         RuleResult result = validator.validate(new PasswordData(password));
+
         if (result.isValid()) {
             return true;
         }
-//        List<String> messages = validator.getMessages(result);
-//        String messageTemplate = String.join(",", messages);
+
         String messageTemplate = "Hasło musi składać się z 8-30 znaków oraz musi zawierać przynajmniej jedną cyfrę";
         context.buildConstraintViolationWithTemplate(messageTemplate)
                 .addConstraintViolation()
